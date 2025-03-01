ea48c7fcb6182e9dfce8278b0f215c5aa5b41945
banyar-shin <banyar.minshin@gmail.com>
2025-03-01 15:11:25 -0800

    set up frameworks

---
+++ backend/server.py
@@ -0,0 +1,27 @@
+import uvicorn
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import StreamingResponse
+from dotenv import load_dotenv
+
+load_dotenv()
+app = FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+@app.get("/")
+async def root():
+    return "Hello World"
+
+if __name__ == "__main__":
+    # Enable hot reload
+    uvicorn.run(
+        "server:app", host="0.0.0.0", port=5000, reload=True, reload_dirs=["./"]
+    )
