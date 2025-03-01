1cdb9a878907187a306a6d002357fdfacb3e8844
AntioniRavioli <anthonylegrama@gmail.com>
2025-03-01 15:31:11 -0800

    fix server not loading windows

--- backend/server.py
+++ backend/server.py
@@ -23,5 +23,5 @@ async def root():
 if __name__ == "__main__":
     # Enable hot reload
     uvicorn.run(
-        "server:app", host="0.0.0.0", port=5000, reload=True, reload_dirs=["./"]
+        "server:app", host="127.0.0.1", port=5000, reload=True, reload_dirs=["./"]
     )
